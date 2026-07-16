e884377c405ee17262e8c20148385cc398cfdafe
meltingseaweed <conor.seo@gmail.com>
2026-07-16 16:31:07 +0900

    Feat: Add function&module to show the to do list item and it's components (except checked)

--- src/index.js
+++ src/index.js
@@ -9,30 +9,27 @@ import Project from "./createProject.js";
 import { ToDos } from "./createToDo.js";
 import { addToDoToProject } from "./addToDoToProject.js";
 import { showProject } from "./showProjects.js";
+import { showToDoList } from "./showToDoList.js";
 
 
 const mainContainer = document.getElementById("project-display");
 const sidebar = document.getElementById("sidebar");
 
 
-const project1 = new Project("title");
-console.log(project1);
+const lifeList = new Project("Life");
+
 const something = new ToDos("eat an avocado");
-console.log(something);
-console.log("Add properties to todo");
+
 addToDoProperties.addDescription(something, "Avocados are healthy so I need to eat one.");
 addToDoProperties.addDueDate(something, "Today");
 addToDoProperties.addPriority(something, "Urgent");
-console.log(something);
-console.log("Add todo to project");
-addToDoToProject(project1, something);
-console.log(project1);
-console.log("Add one more todo");
+
+addToDoToProject(lifeList, something);
+
 const eatFish = new ToDos("eat a fish");
-addToDoToProject(project1, eatFish);
-console.log(project1);
-showProject(project1, mainContainer);
-const project2 = new Project("I LOVE AVOCADOS!!!");
-showProject(project2, mainContainer);
-showProject(project2, sidebar);
-showProject(project1, sidebar);
+addToDoToProject(lifeList, eatFish);
+
+showProject(lifeList, mainContainer);
+
+showProject(lifeList, sidebar);
+showToDoList(lifeList, mainContainer)
---
+++ src/showToDoList.js
@@ -0,0 +1,26 @@
+
+function showToDoList(project, container) {
+
+    container.textContent = "";
+    project.toDos.forEach((item) => {
+        const newItem = document.createElement("article"); 
+
+        Object.entries(item).forEach(([key, val]) => {
+            if (key === "title") {
+                const entry = document.createElement("h2");
+                entry.textContent = `${val}.`
+                console.log(`${item}'s ${key} is ${val}.`)
+                newItem.appendChild(entry);
+            } else if (key != "checkList") {
+                const entry = document.createElement("p");
+                entry.textContent = `${key}: ${val}.`
+                console.log(`${item}'s ${key} is ${val}.`)
+                newItem.appendChild(entry);
+            }
+        });
+        container.appendChild(newItem);
+    }); 
+
+}
+
+export { showToDoList };
